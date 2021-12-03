1a80cf22616420d7fa33b2c2e1362cb7e4dcf8f8
mcangrejo <74432387+mcangrejo@users.noreply.github.com>
2021-12-03 15:11:18 -0500

    Add files via upload

---
+++ miprimercodigo.cpp
@@ -0,0 +1,25 @@
+/*Mi primer programa
+realizado por: Martha Cano  */
+// Los comentarios se pueden realizar con dos slashs o con slash asteristicos
+// el doble slahs solo funciona para una línea de comentarios
+/* el slash asterisco funciona
+para
+varias 
+líneas de comentarios*/
+
+#include <stdio.h>    //en C se deben incluir siempre las librerias a usar
+#include <stdlib.h>
+
+int main()
+{
+	printf("Estoy programando en C");  //Las instrucciones en C terminan con ;
+	system("start http://www.samsung.com"); //instruccion al sistema para abrir una pagina web
+	system("start C:\\Windows\\System32\\Magnify.exe"); //instruccion al sistema para ejecutar una aplicacion 
+	
+	return 0;
+
+}
+
+
+
+
